b8845ee397e2c8a96080a16a730e149fee21f069
naotarian <naotarian19@gmail.com>
2023-01-17 16:34:39 +0900

    first commit

---
+++ resources/js/Pages/hello-world.jsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button'
+const HelloWorld = () => {
+  return (
+      <div>
+        <Button variant='contained'>test</Button>
+          <p>Hello World!</p>
+      </div>
+  );
+}
+export default HelloWorld
